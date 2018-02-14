fe6aaf8cb990b3ea89f9f07c2580617c581a2f06
Enter Name <drichmon@oswego.edu>
2018-02-14 01:12:50 -0500

    Question Pack parser

---
+++ src/sample/QuestionPack.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class QuestionPack {
+
+    ArrayList<String> qArrayList;
+
+
+    public QuestionPack() {
+        qArrayList = new ArrayList<String>();
+
+    }
+
+    public void getQuestionPack(String aFileName) {
+        File q = new File(aFileName);
+        try {
+            Scanner scanner = new Scanner(q);
+            String line = "";
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                qArrayList.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("The file " + q.getPath() + " was not found.");
+        }
+    }
+}
+
+
+
