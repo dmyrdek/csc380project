28b811396398a58617ee12ef1ef0ec35283cd44b
b-minus <bemighol@oswego.edu>
2018-02-27 00:20:36 -0500

    Starting Thread Class

---
+++ src/sample/ServerThread.java
@@ -0,0 +1,44 @@
+package sample;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+    Socket threadSock;
+
+    public ServerThread(Socket clientSock){
+        threadSock =clientSock;
+    }
+
+    public void run(){
+        BufferedReader reader = null;
+        DataOutputStream out = null;
+
+        try{
+            InputStream in = threadSock.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(in));
+            out = new DataOutputStream(threadSock.getOutputStream());
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        String line;
+        while(true){
+            try{
+                line = reader.readLine();
+                if(line != null){
+                    out.writeBytes(line + "\n");
+                    out.flush();
+                }
+                else{
+                    threadSock.close();
+                    return;
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
