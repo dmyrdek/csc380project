9425506bb104bd8abbf09b56e94cb16a9f3e6a8f
Enter Name <drichmon@oswego.edu>
2018-03-03 10:10:30 -0500

    began junit tests

---
+++ src/test/java/csc380Project/TestRunner.java
@@ -0,0 +1,17 @@
+package csc380Project;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(PlayerTest.class, GameTest.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+}
