679a107aac999ffab9dd78d7b8112bade95abece
Enter Name <drichmon@oswego.edu>
2018-03-24 17:13:56 -0400

    removed unnecessary networking methods

--- src/main/java/csc380Project/server/GameClient.java
+++ src/main/java/csc380Project/server/GameClient.java
@@ -157,32 +157,4 @@ public class GameClient {
       System.exit(0);
     }
   }
-
-  public static void connectClient(int port) throws Exception {
-
-    Socket sock = new Socket("tcp://0.tcp.ngrok.io", port);
-    // reading from keyboard (keyRead object)
-    BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-    // sending to client (pwrite object)
-    OutputStream ostream = sock.getOutputStream();
-    PrintWriter pwrite = new PrintWriter(ostream, true);
-
-    // receiving from server ( receiveRead  object)
-    InputStream istream = sock.getInputStream();
-    BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-
-    System.out.println("Start the chitchat, type and press Enter key");
-
-    String receiveMessage, sendMessage;
-    while (true) {
-      sendMessage = keyRead.readLine(); // keyboard reading
-      pwrite.println(sendMessage); // sending to server
-      pwrite.flush(); // flush the data
-      if ((receiveMessage = receiveRead.readLine()) != null) //receive from server
-      {
-        System.out.println(receiveMessage); // displaying at DOS prompt
-      }
-    }
-  }
-
 }
--- src/main/java/csc380Project/server/GameServer.java
+++ src/main/java/csc380Project/server/GameServer.java
@@ -24,37 +24,4 @@ public class GameServer {
 		}
 		
 	}
-
-
-    public static void createServer() throws IOException {
-        ServerSocket sersock = new ServerSocket(4122);
-        System.out.println("Server ready for chatting");
-        Socket sock = sersock.accept( );
-        // reading from keyboard (keyRead object)
-        BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-        // sending to client (pwrite object)
-        OutputStream ostream = sock.getOutputStream();
-        PrintWriter pwrite = new PrintWriter(ostream, true);
-
-        // receiving from server ( receiveRead  object)
-        InputStream istream = sock.getInputStream();
-        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-
-        String receiveMessage, sendMessage;
-        while(true)
-        {
-            if((receiveMessage = receiveRead.readLine()) != null)
-            {
-                System.out.println(receiveMessage);
-            }
-            //
-            pwrite.flush();
-            //
-
-            sendMessage = keyRead.readLine();
-            pwrite.println(sendMessage);
-            pwrite.flush();
-        }
-    }
-
 }
