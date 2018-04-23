33affc2fdea47dd391a0cc98cad8e8185f7089c7
dmyrdek <dmyrdek@oswego.edu>
2018-04-22 20:01:01 -0400

    fix for getting answers 6x

--- src/main/java/csc380Project/server/ClientThread.java
+++ src/main/java/csc380Project/server/ClientThread.java
@@ -271,17 +271,14 @@ public class ClientThread extends Thread {
         synchronized (this) {
           if (line.startsWith("~")) {
             this.answers[currentround][questionNumber] = line.substring(1);
-            if (allPlayersSubmitted){
               for (int i = 0; i < maxClientsCount; i++) {
                 if (this == threads[i]) {
                   threads[0].myGame.getInGamePlayers().get(i).addAnswer(this.answers[currentround][questionNumber],
                       threads[0].myGame.getInGamePlayers().get(i).getQuestionsToAnswerForRound(currentround).get(questionNumber));
                 }
               }
-              allPlayersSubmitted = false;
             }
           }
-        }
 
         // Exiting chat and game
         if (line.startsWith("/quit")) {
