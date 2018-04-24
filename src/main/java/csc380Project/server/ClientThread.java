1ec130593d08b610168820e179813f742036ea58
dmyrdek <dmyrdek@oswego.edu>
2018-04-24 00:30:38 -0400

    I think i fixed it, will this break it?

--- src/main/java/csc380Project/server/ClientThread.java
+++ src/main/java/csc380Project/server/ClientThread.java
@@ -260,8 +260,8 @@ public class ClientThread extends Thread {
                   this.questionNumber = 1;
                 } else if (this.questionNumber == 1){
 
-                  //this.roundsNum++;
-                  //this.questionNumber = 0;
+                  this.roundsNum++;
+                  this.questionNumber = 0;
             } 
           } else if (line.substring(1).equals("inVotingPrompt")){
             this.inVotingPrompt = true;
