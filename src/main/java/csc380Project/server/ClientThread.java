83733341e632a118b4ad4528d74aac6ed7db0f36
dmyrdek <dmyrdek@oswego.edu>
2018-04-24 15:10:55 -0400

    Trying to fix random null pointers when adding players

--- src/main/java/csc380Project/server/ClientThread.java
+++ src/main/java/csc380Project/server/ClientThread.java
@@ -276,6 +276,21 @@ public class ClientThread extends Thread {
 
         synchronized (this) {
           if (getQuestions) {
+            if (this == threads[0]) {
+              for (int i = 0; i < maxClientsCount; i++) {
+                if (threads[i] != null) {
+                  //threads[i].myRounds[1][1] = true;
+                  if (!threads[0].playerList.contains(threads[i].player)) {
+                    threads[0].playerList.add(threads[i].player);
+                  }
+                }
+              }
+              if (myGame == null) {
+                myGame = new Game(10, this.playerList);
+                //System.out.println(myGame.toString());
+              }
+            }
+
             for (int i = 0; i < maxClientsCount; i++) {
               if (threads[i] != null) {
                 threads[i].os.println("{" + threads[0].myGame.getInGamePlayers().get(i)
